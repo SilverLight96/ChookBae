9045604f0f0085f20d4f97777bf7e27adc1aa212
kimsoohwan <soo930810@naver.com>
2022-10-24 17:35:18 +0900

    feature/내브바 기본구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,25 @@ import { BrowserRouter,Route, Routes } from "react-router-dom";
 import MainPage from './Routes/MainPage';
 import LoginPage from './Routes/LoginPage';
 import SignUpPage from './Routes/SignUpPage';
+import ProfilePage from "./Routes/ProfilePage";
+import NavBar from "./Components/common/NavBar";
+import PredictionPage from "./Routes/PredictionPage";
+import PlayerGetPage from "./Routes/PlayerGetPage";
+import MatchInfoPage from "./Routes/MatchInfo";
 
 
 function App() {
   return (
     <BrowserRouter>    
+    <NavBar/>
     <Routes>
         <Route path="" element={<MainPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/prediction" element={<PredictionPage/>}/>
+        <Route path="/playerget" element={<PlayerGetPage/>}/>
+        <Route path="/matchinfo" element={<MatchInfoPage/>}/>
     </Routes> 
   </BrowserRouter>
   );
