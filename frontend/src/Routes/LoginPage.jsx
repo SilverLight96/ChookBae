c7a9789a2392f515edd773316a1ed00991b7cf3d
kimsoohwan <soo930810@naver.com>
2022-10-25 17:51:02 +0900

    feature/signUp 기능 구현

--- frontend/src/Routes/LoginPage.jsx
+++ frontend/src/Routes/LoginPage.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../assets/ChookBae_logo.png";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function LoginPage() {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    setState(data);
+    console.log(state);
+  };
+
   return (
     <Wrapper>
+      <Logo>
+        <img src={logo} alt="로그인 페이지 로고" />
+      </Logo>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">이메일</label>
+        <input
+          id="email"
+          type="text"
+          placeholder="test@email.com"
+          {...register("email", {
+            required: "이메일은 필수 입력 사항입니다.",
+            pattern: {
+              value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              message: "이메일 형식에 맞지 않습니다.",
+            },
+          })}
+        />
+        {errors.email && <small role="alert">{errors.email.message}</small>}
+        <input type="submit" valu="로그인" />
+      </form>
+
       <ChookBae>로그인을 합시다</ChookBae>
     </Wrapper>
   );
@@ -14,6 +53,14 @@ export default LoginPage;
 const Wrapper = styled.div`
   max-width: 860px;
   margin: auto;
+`;
+
+const Logo = styled.div`
+  margin: auto;
+  width: 35%;
+  > img {
+    width: 100%;
+  }
 `;
 
 const ChookBae = styled.header`
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 
 
@@ -12,7 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RecoilRoot>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
       <App />
     </ThemeProvider>
   </RecoilRoot>,
