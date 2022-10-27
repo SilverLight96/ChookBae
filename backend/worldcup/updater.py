8df594cbadf380ec5cff1c2a1940115808346c1c
박상수 <qkftkftnf@naver.com>
2022-10-27 16:09:37 +0900

    💊fix/timezone

--- backend/worldcup/updater.py
+++ backend/worldcup/updater.py
@@ -5,6 +5,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
     scheduler.add_job(predictcalc, 'cron', hour='12')
     scheduler.start()
