58d2b1f4a552f83c68cd3a631d890fdb8db02cc7
박상수 <qkftkftnf@naver.com>
2022-10-27 11:04:35 +0900

    💊feature/check predict

--- backend/worldcup/views.py
+++ backend/worldcup/views.py
@@ -17,29 +17,45 @@ class matchpredict(APIView):
         'predict': openapi.Schema(type=openapi.TYPE_NUMBER, description="승부 예측"),
         })
 
-    def get_object(self, id):
-        try:
-            user=User.objects.get(id=1)
-            match_num=Bet.objects.get(id=match_pk)
+    def get_object(self, match_pk,point,predict):
 
-            if match_num is None:
-                bet=Bet.objects.create(match=Match.objects.get(id=match_pk), win=0, draw=0, lose=0)
+            match=Match.objects.get(id=match_pk)
+            user=User.objects.get(id=1)
+            try:
+                match_num=Bet.objects.get(id=match_pk)
+            except Bet.DoesNotExist:
+                bet=Bet.objects.create(id=match, win=0, draw=0, lose=0)
 
             
-            bet=Bet.objects.get(match=Match.objects.get(id=1))
+            bet=Bet.objects.get(id=match_pk)
             if(predict==0):
                 bet.win+=point
             elif(predict==1):
-                bet.draw+=upoint
+                bet.draw+=point
             else: 
                 bet.lose+=point
 
             bet.save()
+            
+            predict=Prediction.objects.create(user_point=point,predict=predict,match_id=match,user_id=user)
 
-        except User.DoesNotExist:
-            raise Http404 
+       
 
     @swagger_auto_schema(operation_id="승부 예측", operation_description="승부 예측하기", request_body=param)
     def post(self, request, format=None):
-        ingredient = self.get_object(request.user)
-        return Response('success')
+        ingredient = self.get_object(request.data['match_pk'],request.data['point'],request.data['predict'])
+        return Response({200: '배팅 성공'})
+
+
+class predictinfo(APIView):
+    id = openapi.Parameter('id', openapi.IN_PATH, description='match_id', required=True, type=openapi.TYPE_NUMBER)
+    @swagger_auto_schema(operation_id="유저의 승부 예측 여부를 조회", operation_description="제공 받은 토큰 값을 기준으로 유저를 파악하고 해당 유저가 승부 예측을 했는지 확인한다", manual_parameters=[id], responses={200: '조회 성공'})
+    def get(self, request, id):
+        try:
+            predict=Prediction.objects.get(match_id=id ,user_id=1)
+
+            return Response(True,"이미 예측을 하셨습니다.")
+        except Prediction.DoesNotExist:
+            return Response(False,"예측을 하지 않았습니다")
+        except Prediction.MultipleObjectsReturned:
+            return Response({False,'뭐하냐?'})   
