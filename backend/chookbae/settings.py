34de3b9f5c2ef62659de54c979de9e9bae8b5977
Suhwan Im <suwhan1992@gmail.com>
2022-10-28 10:05:46 +0900

    �updaate/requirements.txt

--- backend/chookbae/settings.py
+++ backend/chookbae/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import db_setting
-<<<<<<< backend/chookbae/settings.py
-=======
 
->>>>>>> backend/chookbae/settings.py
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
