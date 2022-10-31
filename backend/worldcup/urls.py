b629548a21cf0ee28eddfceb18ede0972fc56543
박상수 <qkftkftnf@naver.com>
2022-10-31 14:42:26 +0900

    💊fix/url pattern

--- backend/worldcup/urls.py
+++ backend/worldcup/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('predict/<int:id>', views.predictinfo.as_view()),
     path('card', views.card.as_view()),
     path('combine', views.combine.as_view()),
-    path('v1/match/team/<int:id>/', views.MatchInfoByTeam.as_view()),
-    path('v1/match/date/<int:id>/', views.MatchInfoByDate.as_view()),
-    path('v1/match/detail/<int:id>/', views.MatchDetail.as_view()),
-    path('v1/match/teaminfo/<int:id>/', views.TeamInfo.as_view()),
+    path('match/team/<int:id>/', views.MatchInfoByTeam.as_view()),
+    path('match/date/<int:id>/', views.MatchInfoByDate.as_view()),
+    path('match/detail/<int:id>/', views.MatchDetail.as_view()),
+    path('match/teaminfo/<int:id>/', views.TeamInfo.as_view()),
 ]
