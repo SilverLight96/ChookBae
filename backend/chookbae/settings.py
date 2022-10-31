230785e67eb130465b249c3d41affbe5321cb21f
jelee6613 <jelee6613@gmail.com>
2022-10-31 11:10:08 +0900

    FEAT : settings.py db_setting 경로

--- backend/chookbae/settings.py
+++ backend/chookbae/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import db_setting
+from secret import db_setting
 # import environ
 import email_setting
 # env = environ.Env(
