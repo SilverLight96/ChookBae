4565e66938d165ebf2eb8b0b3a9231f51d98b5ec
박상수 <qkftkftnf@naver.com>
2022-11-03 16:29:51 +0900

    💊feature/translate playername

--- backend/worldcup/Serializers.py
+++ backend/worldcup/Serializers.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
-from .models import Player,User,Match
+from .models import User,Match
 
-
-
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Player
-        fields = ( 'player_image', 'fullname', 'value', )
 
 
 class UserrankSerializer(serializers.ModelSerializer):
@@ -14,10 +8,6 @@ class UserrankSerializer(serializers.ModelSerializer):
         model= User
         fields = ( 'nickname', 'value', )
 
-class goalrankSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Player
-        fields = ( 'fullname', 'goal', 'value',)
 
 class matchidSerializer(serializers.ModelSerializer):
     class Meta:
